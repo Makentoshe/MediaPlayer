6356464fabd4bcaf891eeb3a3c93d2cd21a26cb0
Makentoshe <mkliverout@gmail.com>
2019-05-16 15:38:21 +0300

    [feature] Call PauseCommand when media playing was ended

--- app/src/main/java/com/makentoshe/vkinternship/player/PlayerService.kt
+++ app/src/main/java/com/makentoshe/vkinternship/player/PlayerService.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Player.STATE_ENDED
 import com.makentoshe.vkinternship.Mp3FilesHolder
 import com.makentoshe.vkinternship.player.commandexec.*
 
--- app/src/main/java/com/makentoshe/vkinternship/player/commandexec/CallbackCommandExecutor.kt
+++ app/src/main/java/com/makentoshe/vkinternship/player/commandexec/CallbackCommandExecutor.kt
@@ -5,8 +5,7 @@ import com.google.android.exoplayer2.Player
 import com.makentoshe.vkinternship.player.CallbackSender
 import com.makentoshe.vkinternship.player.Commands
 
-class CallbackCommandExecutor(private val callback: CallbackSender) :
-    CommandExecutor {
+class CallbackCommandExecutor(private val callback: CallbackSender) : CommandExecutor {
 
     override fun exec(mediaPlayer: ExoPlayer) {
         exec(mediaPlayer.playWhenReady, mediaPlayer.playbackState)
@@ -15,5 +14,6 @@ class CallbackCommandExecutor(private val callback: CallbackSender) :
     fun exec(isPlaying: Boolean, state: Int) {
         if (isPlaying) callback.send(Commands.PlayCommand) else callback.send(Commands.PauseCommand)
         if (state == Player.STATE_IDLE) callback.send(Commands.IdleStateCommand)
+        if (state == Player.STATE_ENDED) callback.send(Commands.PauseCommand)
     }
 }
