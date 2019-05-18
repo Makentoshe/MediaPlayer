66e114ac92f2626902398fd559a6d22d9457eb57
Makentoshe <mkliverout@gmail.com>
2019-05-18 17:54:51 +0300

    [refactor] update backdrop background

--- app/src/main/java/com/makentoshe/vkinternship/BackdropBackgroundController.kt
+++ app/src/main/java/com/makentoshe/vkinternship/BackdropBackgroundController.kt
@@ -1,5 +1,7 @@
 package com.makentoshe.vkinternship
 
+import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.FragmentManager
@@ -16,6 +18,14 @@ class BackdropBackgroundController(
         folderButton.setOnClickListener {
             //request permission using fragment
             fragmentManager.beginTransaction().add(PermissionFragment(), String()).commit()
+        }
+
+        background.setOnTouchListener(OnSwipeTouchListenerImpl(background.context))
+    }
+
+    private class OnSwipeTouchListenerImpl(private val context: Context): OnSwipeTouchListener(context) {
+        override fun onSwipeBottom() {
+            (context as Activity).moveTaskToBack(false)
         }
     }
 }
